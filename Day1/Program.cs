9e855d0823a5d022d77ea1f5d5f6911ff15463a4
edwardreeve <edreeve@gmail.com>
2021-12-18 16:11:03 +0000

    adding day 1 deets and solution

---
+++ Day1/Program.cs
@@ -0,0 +1,31 @@
+﻿var data = File.ReadLines("Day1Data.txt")
+    .Select(entry => int.Parse(entry))
+    .ToArray();
+
+void Part1()
+{
+    var counter = 0;
+    for (int i = 1; i < data.Count(); i++)
+    {
+        if (data[i] > data[i - 1]) counter += 1;
+    }
+
+    Console.WriteLine($"Part 1 answer is {counter}");
+}
+
+void Part2()
+{
+    var previousTotal = 0;
+    var counter = 0;
+    for (int i = 1; i < data.Length - 2; i++)
+    {
+        var total = data[i..(i + 3)].Sum();
+        if (total > previousTotal) counter += 1;
+        previousTotal = total;
+    }
+
+    Console.WriteLine($"Part 2 answer is {counter}");
+}
+
+Part1();
+Part2();
