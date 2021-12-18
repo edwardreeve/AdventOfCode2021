16f6ab5a7f5e28145c7690b94eb3d5b00f66ff62
edwardreeve <edreeve@gmail.com>
2021-12-18 20:25:25 +0000

    hacky solution to day3 part 1

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -1,8 +1,43 @@
-﻿var data = File.ReadLines("Day3Example.txt")
+﻿using System.Xml.Schema;
+
+var data = File.ReadLines("Day3Data.txt")
     .Select(line => line.ToArray())
     .ToArray();
 
 void Part1()
 {
+    var numberOfLines = data.Length;
+    var numberOfBits = data.First().Length;
+    var aggregate = new char[numberOfBits, numberOfLines];
+    for (int i = 0; i < numberOfLines; i++)
+    {
+        for (int j = 0; j < numberOfBits; j++)
+        {
+            aggregate[j, i] = data[i][j];
+        }
+    }
+
+    var gammaString = string.Empty;
     
+    for (int i = 0; i < numberOfBits; i++)
+    {
+        var commonCharacter = Enumerable.Range(0, numberOfLines)
+            .Select(x => aggregate[i, x])
+            .ToArray()
+            .GroupBy(character => character)
+            .OrderByDescending(grouping =>grouping.Count())
+            .Select(grouping => grouping.Key)
+            .First();
+
+        gammaString += commonCharacter;
+    }
+
+    var epsilonString = new string(gammaString.Select(character => character == '1' ? '0' : '1').ToArray());
+    var gamma = Convert.ToInt32(gammaString, 2);
+    var epsilon = Convert.ToInt32(epsilonString, 2);
+
+    Console.WriteLine($"The answer for part 1 is gamma({gamma}) x epsilon" +
+                      $"({epsilon}) = {gamma * epsilon}");
 }
+
+Part1();
