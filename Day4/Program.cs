ebf26fdc6912149082b0707c2586a685a9b6ccc7
edwardreeve <edreeve@gmail.com>
2021-12-20 16:16:29 +0000

    initial creation of bingo calls and boards

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿var sourceData = File.ReadLines("Day4Example.txt").ToArray();
 
-Console.WriteLine("Hello, World!");
+void Part1()
+{
+    var bingoCalls = sourceData.First().Split(",");
+
+    var bingoBoards = sourceData.Take(1..)
+        .Where(dataRow => !string.IsNullOrEmpty(dataRow))
+        .Chunk(5)
+        .Select(boardData => new BingoBoard(boardData));
+}
+
+Part1();
+
+public class BingoBoard
+{
+    private string[] BoardData;
+    public BingoBoard(string[] boardData)
+    {
+        BoardData = boardData;
+    }
+}
