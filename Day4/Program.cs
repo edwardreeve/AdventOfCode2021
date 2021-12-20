e4c5a9acac23f78bdc3c750c8b2b5c0efd64f218
edwardreeve <edreeve@gmail.com>
2021-12-20 17:22:20 +0000

    mark a number on a bingo board

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -8,15 +8,40 @@ void Part1()
         .Where(dataRow => !string.IsNullOrEmpty(dataRow))
         .Chunk(5)
         .Select(boardData => new BingoBoard(boardData));
+
+    foreach (var call in bingoCalls)
+    {
+        foreach (var board in bingoBoards)
+        {
+            board.MarkCall(call);
+        }
+    }
 }
 
 Part1();
 
 public class BingoBoard
 {
-    private string[] BoardData;
+    private List<int> _boardData;
+    private List<int> _results;
+    private int _matchedNumber = -1;
+
     public BingoBoard(string[] boardData)
     {
-        BoardData = boardData;
+        var allData = string.Join(" ", boardData);
+        _boardData = allData.Split(" ")
+            .Where(x => !string.IsNullOrEmpty(x))
+            .Select(x => int.Parse(x)).ToList();
+        _results = _boardData.ToList();
+    }
+
+    public void MarkCall(string call)
+    {
+        var callNumber = int.Parse(call);
+        if (_boardData.Contains(callNumber))
+        {
+            var index = _results.IndexOf(callNumber);
+            _results[index] = _matchedNumber;
+        }
     }
 }
