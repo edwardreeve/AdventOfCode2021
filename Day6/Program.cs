08c545ecace3d22ea4c90bc09d59c0aa2c4920dc
edwardreeve <edreeve@gmail.com>
2021-12-22 19:57:57 +0000

    array shifting solution for day 6 part 2

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -15,17 +15,67 @@ void Part1(int days)
     {
         totalFish += fish.IncreaseAge(days).CountFamily();
     }
+
     Console.WriteLine($"After {days} days, there would be {totalFish} lanternfish");
 }
 
+void Attempt3(int days)
+{
+    // var fishAges = Enumerable.Range(0, 9)
+    //     .Select(x => new
+    //     {
+    //         Age = x,
+    //         Count = sourceData.Count(fishAge => fishAge == x)
+    //     })
+    //     .OrderByDescending(x => x.Age)
+    //     .ToList();
+
+    // var fishAges = new List<(int Age, int Count)>();
+    // for (int i = 8; i >= 0; i--)
+    // {
+    //     fishAges.Add((i, sourceData.Count(fishAge => fishAge == i)));
+    // }
+    //
+    // for (int i = 0; i < days; i++)
+    // {
+    //     foreach (var fishAge in fishAges)
+    //     {
+    //         fishAges[fishAge.Age].Count = fishAge.Count;
+    //     }
+    // }
+
+    var fishAgeArray = new long[9];
+    var groupedFishAges = sourceData.GroupBy(x => x);
+    foreach (var group in groupedFishAges)
+    {
+        fishAgeArray[group.Key] = group.Count();
+    }
+
+    for (int i = 0; i < days; i++)
+    {
+        var newFish = fishAgeArray[0];
+        fishAgeArray[0] = fishAgeArray[1];
+        fishAgeArray[1] = fishAgeArray[2];
+        fishAgeArray[2] = fishAgeArray[3];
+        fishAgeArray[3] = fishAgeArray[4];
+        fishAgeArray[4] = fishAgeArray[5];
+        fishAgeArray[5] = fishAgeArray[6];
+        fishAgeArray[6] = fishAgeArray[7] + newFish;
+        fishAgeArray[7] = fishAgeArray[8];
+        fishAgeArray[8] = newFish;
+    }
+    
+    Console.WriteLine($"After {days} days, there would be {fishAgeArray.Sum(x => x)} lanternfish");
+}
+
 // Part1(80);
-Part1(256);
+Attempt3(256);
 
 public class Fish
 {
     private int _daysUntilBirth;
     private readonly List<Fish> _children = new();
-    
+
     public Fish(int daysUntilBirth)
     {
         _daysUntilBirth = daysUntilBirth;
@@ -60,6 +110,7 @@ public class Fish
         {
             child.AddDay();
         }
+
         if (giveBirth) _children.Add(new Fish(8));
     }
 }
