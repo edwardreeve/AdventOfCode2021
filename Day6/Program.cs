f7bdec3593f1f8172cf0dd0ff2f264e01a6f59e9
EdReeve <ed.reeve@exizent.com>
2021-12-22 09:57:55 +0000

    added notes to come back to

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -6,6 +6,9 @@
 
 void Part1(int days)
 {
+    // not working. same issue.
+    // there are only 9 possible values that a fish can have
+    // find a way to calculate how many fish are in each of those positions after each day
     var fishes = sourceData.Select(x => new Fish(x));
     ulong totalFish = 0;
     foreach (var fish in fishes)
@@ -15,15 +18,14 @@ void Part1(int days)
     Console.WriteLine($"After {days} days, there would be {totalFish} lanternfish");
 }
 
-
-
-Part1(80);
-// Part1(256);
+// Part1(80);
+Part1(256);
 
 public class Fish
 {
     private int _daysUntilBirth;
-    private List<Fish> _children = new();
+    private readonly List<Fish> _children = new();
+    
     public Fish(int daysUntilBirth)
     {
         _daysUntilBirth = daysUntilBirth;
