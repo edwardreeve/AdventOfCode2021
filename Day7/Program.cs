0983d1336b028034ee051d67f1ddab368a4c3ed0
edwardreeve <edreeve@gmail.com>
2021-12-22 20:47:35 +0000

    solution to day 7 part 1

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -1,3 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+var sourceData = File.ReadAllText("Day7Data.txt")
+    .Trim()
+    .Split(",")
+    .Select(int.Parse);
+
+void Part1()
+{
+    var halfIndex = sourceData.Count() / 2;
+    var sortedCrabs = sourceData.OrderBy(n=>n).ToList();
+    var median = sourceData.Count() % 2 == 0
+        ? (sortedCrabs[halfIndex] + sortedCrabs[halfIndex - 1]) / 2
+        : sortedCrabs[halfIndex];
+
+    var fuelUsed = 0;
+    foreach (var crab in sourceData)
+    {
+        fuelUsed += Math.Abs(crab - median);
+    }
+    
+    Console.WriteLine($"The answer for part 1 is {fuelUsed}");
+}
+
+Part1();
