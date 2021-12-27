c2fefa57163ad911e36794b837f28b732e16a55f
edwardreeve <edreeve@gmail.com>
2021-12-27 11:15:04 +0000

    remove old part 1 method

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -3,29 +3,21 @@
     .Split(",")
     .Select(int.Parse);
 
+var minCrab = sourceData.Min();
+var maxCrab = sourceData.Max();
+var positionsInRange = maxCrab - minCrab + 1;
+
 void Part1()
 {
-    var midpoint = sourceData.Count() / 2;
-    var sortedCrabs = sourceData.OrderBy(n=>n).ToList();
-    var median = sourceData.Count() % 2 == 0
-        ? (sortedCrabs[midpoint] + sortedCrabs[midpoint - 1]) / 2
-        : sortedCrabs[midpoint];
+    var minFuelUsed = Enumerable.Range(minCrab, positionsInRange)
+        .Select(x => sourceData.Select(d => Math.Abs(d - x)).Sum())
+        .Min();
 
-    var fuelUsed = sourceData.Sum(x => Math.Abs(x - median));
-    
-    Console.WriteLine($"The answer for part 1 is {fuelUsed}");
+    Console.WriteLine($"The answer for part 1 is {minFuelUsed}");
 }
 
-// Tried doing the equivalent as part one, but by calculating the mean instead of the median
-// Didn't work. Reddit says there are rounding errors. Trying another way.
 void Part2()
 {
-    var minCrab = sourceData.Min();
-    var maxCrab = sourceData.Max();
-    var positionsInRange = maxCrab - minCrab + 1;
-    var answer = Enumerable.Range(minCrab, positionsInRange)
-        .Select(x => sourceData.Select(d => Math.Abs(d - x)).Sum()).Min();
-    Console.WriteLine($"Alternative method question 1 answer = {answer}");
 }
 
 Part1();
