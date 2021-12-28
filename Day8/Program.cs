bc868458c89c787a7787554feb9d91606a863a62
edwardreeve <edreeve@gmail.com>
2021-12-28 15:02:40 +0000

    solution to day 8 part 1

---
+++ Day8/Program.cs
@@ -0,0 +1,26 @@
+﻿var sourceData = File.ReadLines("Day8Data.txt")
+    .Select(x => x.Split("|")[1]
+        .Trim()
+        .Split(" ")
+        .ToArray())
+    .ToList();
+
+void Part1()
+{
+    var result = GetUniqueDigits(sourceData);
+    Console.WriteLine($"The answer to part 1 is {result}");
+}
+
+int GetUniqueDigits(List<string[]>? input)
+{
+    int[] sectionsInUniqueDigits =
+    {
+        2,
+        3,
+        4,
+        7
+    };
+    return input.Select(x => x.Count(y => sectionsInUniqueDigits.Contains(y.Length))).Sum();
+}
+
+Part1();
