b9143d2f601028943242060e82b4549aff5096af
edwardreeve <edreeve@gmail.com>
2021-12-30 15:12:28 +0000

    working solution to day 9 part 1 with bad model, will refactor

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,11 +1,11 @@
 ﻿var data = File.ReadLines("Day1Data.txt")
-    .Select(entry => int.Parse(entry))
+    .Select(int.Parse)
     .ToArray();
 
 void Part1()
 {
     var counter = 0;
-    for (int i = 1; i < data.Count(); i++)
+    for (var i = 1; i < data.Length; i++)
     {
         if (data[i] > data[i - 1]) counter += 1;
     }
@@ -17,7 +17,7 @@ void Part2()
 {
     var previousTotal = 0;
     var counter = 0;
-    for (int i = 1; i < data.Length - 2; i++)
+    for (var i = 1; i < data.Length - 2; i++)
     {
         var total = data[i..(i + 3)].Sum();
         if (total > previousTotal) counter += 1;
--- Day9/Program.cs
+++ Day9/Program.cs
@@ -1,3 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿var sourceData = File.ReadLines("Day9Data.txt")
+    .Select(x => x.ToArray()
+        .Select(c => new depthRecord(c))
+        .ToArray())
+    .ToArray();
 
-Console.WriteLine("Hello, World!");
+void Part1()
+{
+    for (var i = 0; i < sourceData.Length; i++)
+    {
+        var sourceRow = sourceData[i];
+        for (var j = 0; j < sourceRow.Length; j++)
+        {
+            var record = sourceRow[j];
+            var lowest = true;
+            if (j > 0) // horizontal; check to the left if we're past the zero index
+            {
+                if (sourceRow[j - 1].Depth <= record.Depth)
+                {
+                    lowest = false;
+                }
+            }
+
+            if (j < sourceRow.Length - 1) // horizontal; check to the right if we're not at the final row
+            {
+                if (sourceRow[j + 1].Depth <= record.Depth)
+                {
+                    lowest = false;
+                }
+            }
+
+            if (i > 0) // vertical; check above if we're past the zero index
+            {
+                if (sourceData[i - 1][j].Depth <= record.Depth)
+                {
+                    lowest = false;
+                }
+            }
+
+            if (i < sourceData.Length - 1) // vertical; check the next if we're not at the final row
+            {
+                if (sourceData[i + 1][j].Depth <= record.Depth)
+                {
+                    lowest = false;
+                }
+            }
+
+            record.Lowest = lowest;
+        }
+    }
+
+    var result = sourceData.SelectMany(row => row)
+        .Where(record => record.Lowest.Value)
+        .Select(x => x.Depth + 1)
+        .Sum();
+
+    Console.WriteLine($"The answer to part one is {result}");
+}
+
+Part1();
+
+class depthRecord
+{
+    public int Depth { get; }
+    public bool? Lowest { get; set; } = null;
+
+    public depthRecord(char depth)
+    {
+        Depth = int.Parse(depth.ToString());
+    }
+}
